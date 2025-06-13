4ba6828d47269e22719520712275b4e435136794
Faisal Sayyed <sayyedfaisal06@gmail.com>
2025-06-13 19:54:07 +0530

    test: add test for single number input

--- src/__tests__/StringCalculator.test.ts
+++ src/__tests__/StringCalculator.test.ts
@@ -10,4 +10,8 @@ describe("StringCalculator", () => {
   test("should return 0 for empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  test("returns number for single value", () => {
+    expect(calculator.add("5")).toBe(5);
+  });
 });
