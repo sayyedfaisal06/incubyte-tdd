dd57122006c2d28b1eca35d77bd26845cccbf7b3
Faisal Sayyed <sayyedfaisal06@gmail.com>
2025-06-13 19:56:37 +0530

    feat: return sum of two numbers separated by comma

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,6 +1,7 @@
 export class StringCalculator {
   add(numbers: string): number {
     if (!numbers) return 0;
-    return parseInt(numbers);
+    const numberArray = numbers.split(",");
+    return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
